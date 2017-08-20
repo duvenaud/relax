993b488ba331e5ad42bab3c59121dddb3a2b9954
David Duvenaud <dduvenaud@seas.harvard.edu>
2017-08-19 21:07:57 -0400

    Slight cleanup

--- rebar/rebar.py
+++ rebar/rebar.py
@@ -5,9 +5,9 @@ from autograd import grad, primitive, value_and_grad
 def heaviside(z):
     return z >= 0
 
-def relaxed_heaviside(z, lam):
+def relaxed_heaviside(z, temperature):
     # sigma_lambda in REBAR paper.
-    return expit(z / lam)
+    return expit(z / temperature)
 
 def logistic_sample(logit_theta, noise_samples):
     # REBAR's z = g(theta, u)
@@ -16,8 +16,8 @@ def logistic_sample(logit_theta, noise_samples):
 def bernoulli_sample(logit_theta, noise_samples):
     return heaviside(logistic_sample(logit_theta, noise_samples))
 
-def relaxed_bernoulli_sample(logit_theta, lam, noise_samples):
-    return relaxed_heaviside(logistic_sample(logit_theta, noise_samples), lam)
+def relaxed_bernoulli_sample(logit_theta, noise_samples, temperature):
+    return relaxed_heaviside(logistic_sample(logit_theta, noise_samples), temperature)
 
 def conditional_noise(logit_theta, samples, noise):
     # The output of this function can be fed into logistic_sample to get g tilde.
@@ -38,7 +38,7 @@ def reinforce(params, noise, f):
     return f(params, samples)
 
 def reinforce_vjp(g, func_vals, vs, gvs, params, noise, f):
-    # We define REINFORCE as the gradient of the above function.
+    # We implement the gradient using the reinforce estimator.
     samples = bernoulli_sample(params, noise)
     grad_func_vals = grad(f)(params, samples)
     grad_logprobs  = grad(bernoulli_logprob)(params, samples)
@@ -49,8 +49,8 @@ reinforce.defvjp(reinforce_vjp)
 ############### CONCRETE ###################
 
 def concrete(params, temperature, noise, f):
-    # Concrete sampling and evaluation is fully differentiable.
-    relaxed_samples = relaxed_bernoulli_sample(params, temperature, noise)
+    # Concrete sampling and evaluation is already differentiable.
+    relaxed_samples = relaxed_bernoulli_sample(params, noise, temperature)
     return f(params, relaxed_samples)
 
 
@@ -58,7 +58,6 @@ def concrete(params, temperature, noise, f):
 
 @primitive
 def rebar(params, temperature, noise, noise2, f):
-    # This is the same function as in reinforce, but we'll give it a different gradient.
     samples = bernoulli_sample(params, noise)
     return f(params, samples)
 
@@ -68,13 +67,13 @@ def rebar_vjp(g, f_vals, vs, gvs, params, temperature, noise, noise2, f):
     def concrete_cond(params):
         # This closure captures the dependency of the conditional samples on params.
         cond_noise = conditional_noise(params, samples, noise2)
-        cond_relaxed_samples = relaxed_bernoulli_sample(params, temperature, cond_noise)
+        cond_relaxed_samples = relaxed_bernoulli_sample(params, cond_noise, temperature)
         return f(params, cond_relaxed_samples)
 
-    grad_func = grad(f)(params, samples)                                # grad through f
-    grad_logprobs = grad(bernoulli_logprob)(params, samples)            # grad through p(b)
-    grad_concrete = grad(concrete)(params, temperature, noise, f)       # grad through z
-    f_cond, grad_concrete_cond = value_and_grad(concrete_cond)(params)  # grad through z tilde
+    grad_func = grad(f)(params, samples)                                # d_f(b) / d_theta
+    grad_logprobs = grad(bernoulli_logprob)(params, samples)            # d_log_p(b) / d_theta
+    grad_concrete = grad(concrete)(params, temperature, noise, f)       # d_f(z) / d_theta
+    f_cond, grad_concrete_cond = value_and_grad(concrete_cond)(params)  # d_f(ztilde) / d_theta
 
     return g * ((f_vals - f_cond) * grad_logprobs + grad_concrete - grad_concrete_cond + grad_func)
 
