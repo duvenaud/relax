90e1eb799006b41330343e208289b590edbfe394
Dami Choi <dami.choi@mail.utoronto.ca>
2017-12-06 18:57:06 -0500

    took out axis label and corrected sign

--- display_grads.py
+++ display_grads.py
@@ -3,7 +3,7 @@ import matplotlib
 import numpy as np
 import cPickle as pickle
 
-with open('10k_mnist_vae_grad_samples.pkl') as f:
+with open('10k_mnist_vae_grad_samples.pkl') as f: #10k_toy_grad_samples.pkl  10k_mnist_vae_grad_samples.pkl
     results = pickle.load(f)
     
 reinf_fs, reinf_cs, rep_cs, zlaxs = results
@@ -30,29 +30,33 @@ plt.hist(reinf_fs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=
 plt.xlim(min_val, max_val)
 plt.title(r'$\hat g_{REINFORCE}[f]$')
 plt.xlabel('unbiased \nhigh variance')
+plt1.axes.yaxis.set_ticks([])
 plt2 = plt.subplot(4, 4, 2)
 plt.hist(reinf_cs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=0.75)
 plt.xlim(min_val, max_val)
 plt.title(r'$\hat g_{REINFORCE}[c_\phi]$')
 plt.xlabel('biased \nhigh variance')
-plt.ylabel('-', rotation=0, size=40)
-plt2.yaxis.set_label_coords(-0.22,0.4)
+plt.ylabel('-', rotation=0, size=50)
+plt2.yaxis.set_label_coords(-0.125,0.4)
+plt2.axes.yaxis.set_ticks([])
 plt3 = plt.subplot(4, 4, 3)
 plt.hist(rep_cs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=0.75)
 plt.xlim(min_val, max_val)
 plt.title(r'$\hat g_{reparam}[c_\phi]$')
 plt.xlabel('biased \nlow variance')
 plt.ylabel(r'+', rotation=0, size=40)
-plt3.yaxis.set_label_coords(-0.2,0.4)
+plt3.yaxis.set_label_coords(-0.13,0.4)
+plt3.axes.yaxis.set_ticks([])
 plt4 = plt.subplot(4, 4, 4)
 plt.hist(zlaxs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=0.75)
 plt.xlim(min_val, max_val)
 plt.title(r'$\hat g_{LAX}$')
 plt.xlabel('unbiased \nlow variance')
 plt.ylabel('=', rotation=0, size=40)
-plt4.yaxis.set_label_coords(-0.2,0.4)
+plt4.yaxis.set_label_coords(-0.13,0.4)
+plt4.axes.yaxis.set_ticks([])
 plt.tight_layout()
-plt.savefig('./plot.pdf', format="pdf", bbox_inches='tight')
+plt.savefig('./10k_mnist_vae_grad_hist.pdf', format="pdf", bbox_inches='tight')
 plt.show()
 
 
