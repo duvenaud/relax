5b37a6ad05c092434b51e2cc47d146f151e155a8
Will Grathwohl <willgrathwohl@WillsMacBookPro.local>
2018-02-21 14:37:11 -0500

    related

--- display_grads.py
+++ display_grads.py
@@ -3,7 +3,8 @@ import matplotlib
 import numpy as np
 import pickle
 
-with open('10k_mnist_vae_grad_samples.pkl') as f: #10k_toy_grad_samples.pkl  10k_mnist_vae_grad_samples.pkl
+fn = '100_samples.pkl'
+with open(fn, 'r') as f:
     results = pickle.load(f)
     
 reinf_fs, reinf_cs, rep_cs, zlaxs = results
@@ -25,36 +26,42 @@ print("++++++++++++++++++++++++++")
 matplotlib.rcParams.update({'font.size': 20})
 
 plt.figure(1, figsize=(20,20))
-plt1 = plt.subplot(4, 4, 1)
+
+plt1 = plt.subplot(4, 4, 2)
 plt.hist(reinf_fs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=0.75)
 plt.xlim(min_val, max_val)
-plt.title(r'$\hat g_{REINFORCE}[f]$')
+plt.title(r'$\hat g_{REINFORCE}[f]$', y=1.025)
 plt.xlabel('unbiased \nhigh variance')
+plt.ylabel('=', rotation=0, size=40)
+plt1.yaxis.set_label_coords(-0.13,0.4)
 plt1.axes.yaxis.set_ticks([])
-plt2 = plt.subplot(4, 4, 2)
+
+plt2 = plt.subplot(4, 4, 3)
 plt.hist(reinf_cs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=0.75)
 plt.xlim(min_val, max_val)
-plt.title(r'$\hat g_{REINFORCE}[c_\phi]$')
+plt.title(r'$\hat g_{REINFORCE}[c_\phi]$', y=1.025)
 plt.xlabel('biased \nhigh variance')
-plt.ylabel('-', rotation=0, size=50)
+plt.ylabel('+', rotation=0, size=40)
 plt2.yaxis.set_label_coords(-0.125,0.4)
 plt2.axes.yaxis.set_ticks([])
-plt3 = plt.subplot(4, 4, 3)
+
+plt3 = plt.subplot(4, 4, 4)
 plt.hist(rep_cs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=0.75)
 plt.xlim(min_val, max_val)
-plt.title(r'$\hat g_{reparam}[c_\phi]$')
+plt.title(r'$\hat g_{reparam}[c_\phi]$', y=1.025)
 plt.xlabel('biased \nlow variance')
-plt.ylabel(r'+', rotation=0, size=40)
-plt3.yaxis.set_label_coords(-0.13,0.4)
+plt.ylabel(r'-', rotation=0, size=60)
+plt3.yaxis.set_label_coords(-0.13,0.30)
 plt3.axes.yaxis.set_ticks([])
-plt4 = plt.subplot(4, 4, 4)
+
+plt4 = plt.subplot(4, 4, 1)
 plt.hist(zlaxs, 50, range=(min_val, max_val), normed=1, facecolor='g', alpha=0.75)
 plt.xlim(min_val, max_val)
-plt.title(r'$\hat g_{LAX}$')
+plt.title(r'$\hat g_{LAX}$', y=1.025)
 plt.xlabel('unbiased \nlow variance')
-plt.ylabel('=', rotation=0, size=40)
-plt4.yaxis.set_label_coords(-0.13,0.4)
 plt4.axes.yaxis.set_ticks([])
+
+
 plt.tight_layout()
 plt.savefig('./10k_mnist_vae_grad_hist.pdf', format="pdf", bbox_inches='tight')
 plt.show()
